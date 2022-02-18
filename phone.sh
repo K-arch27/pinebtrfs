5bed5692310ff16d5589fe609a33eaa0ebfa1019
K-arch27 <98610690+K-arch27@users.noreply.github.com>
2022-02-17 22:15:26 -0500

    Create phone.sh

---
+++ phone.sh
@@ -0,0 +1,33 @@
+#to be run on the phone after booting on it
+
+pacman -Syyu
+pacman -S snapper
+
+
+umount /.snapshots
+rm -r /.snapshots
+snapper --no-dbus -c root create-config /
+btrfs subvolume delete /.snapshots
+mkdir /.snapshots
+mount -a
+chmod 750 /.snapshots
+
+
+
+
+#Changing The timeline auto-snap
+sed -i 's|QGROUP=""|QGROUP="1/0"|' /etc/snapper/configs/root
+sed -i 's|NUMBER_LIMIT="50"|NUMBER_LIMIT="10-15"|' /etc/snapper/configs/root
+sed -i 's|NUMBER_LIMIT_IMPORTANT="50"|NUMBER_LIMIT_IMPORTANT="5-10"|' /etc/snapper/configs/root
+sed -i 's|TIMELINE_LIMIT_HOURLY="10"|TIMELINE_LIMIT_HOURLY="0"|' /etc/snapper/configs/root
+sed -i 's|TIMELINE_LIMIT_DAILY="10"|TIMELINE_LIMIT_DAILY="3"|' /etc/snapper/configs/root
+sed -i 's|TIMELINE_LIMIT_WEEKLY="0"|TIMELINE_LIMIT_WEEKLY="2"|' /etc/snapper/configs/root
+sed -i 's|TIMELINE_LIMIT_MONTHLY="10"|TIMELINE_LIMIT_MONTHLY="2"|' /etc/snapper/configs/root
+sed -i 's|TIMELINE_LIMIT_YEARLY="10"|TIMELINE_LIMIT_YEARLY="0"|' /etc/snapper/configs/root
+
+
+
+#Enable cleanup
+
+systemctl enable snapper-timeline.timer
+systemctl enable snapper-cleanup.timer
